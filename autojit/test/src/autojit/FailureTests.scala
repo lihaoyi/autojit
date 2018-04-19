cd67b6e989e325bd4e95d2bff782e05b7d93531d
Li Haoyi <haoyi.sg@gmail.com>
2018-04-18 18:58:22 -0700

    Add failure unit test for passing `this` to external methods

--- autojit/test/src/autojit/FailureTests.scala
+++ autojit/test/src/autojit/FailureTests.scala
@@ -3,13 +3,23 @@ package autojit
 import utest._
 sealed trait Failure{ def eval(): Int }
 object Failure{
-  case class Num(i: Int) extends Failure{
+  case class Lambda(i: Int) extends Failure{
     // This fails because we aren't smart enough to handle the InvokeDynamic
     // call, which references `this` in order to call the bootstrap method
     def eval() = Some(1).map(_ => i).get
   }
+  case class External(i: Int) extends Failure{
+    // This fails because we aren't smart enough to handle the InvokeDynamic
+    // call, which references `this` in order to call the bootstrap method
+    def eval() = external(this)
+  }
   case class Inc(l: Failure) extends Failure{
     def eval() = l.eval() + 1
+  }
+
+  def external(x: Any) = {
+    println(x)
+    1337
   }
 }
 
@@ -18,7 +28,13 @@ object FailureTests extends TestSuite {
   def tests = Tests{
     'hello0 - {
       val ex = intercept[java.lang.Exception]{
-        Lib.devirtualize[Failure](Num(1), "eval").eval()
+        Lib.devirtualize[Failure](Lambda(1), "eval").eval()
+      }
+      assert(ex.getMessage == "Non-inlined self reference slipped through")
+    }
+    'hello1 - {
+      val ex = intercept[java.lang.Exception]{
+        Lib.devirtualize[Failure](External(1), "eval").eval()
       }
       assert(ex.getMessage == "Non-inlined self reference slipped through")
     }
